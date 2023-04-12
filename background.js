47d86e0e56bb620663395b3ac7fd12e416a58ef7
Kirill Shaferov <kirill_shaferov@mail.ru>
2023-04-13 01:01:21 +0300

    Обновленная логика перебора каналов

--- background.js
+++ background.js
@@ -1,18 +1,67 @@
-let userChannel = ''
-
-chrome.runtime.onMessage.addListener(({oper,listToServer},sendResponse) =>
-{
-    //debugger;
-    switch(oper){
-        case "sendMessageBack":{
-            userChannel = listToServer;
-            console.log(userChannel)
-            break;
+//const sleep = ms => new Promise(r => setTimeout(r, ms));
+(async () => {
+    let userChannel = '';
+    let tg_channel = [];
+    
+    async function getTabId(){
+        var tabId = -1;
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+            tabs.forEach(async function (tab) {
+                if (tab.url.indexOf('telegram') !== -1) {
+                    //debugger;
+                    tabId = tab.id;
+                }
+            });
+        });
+        return tabId;
+    }
+    async function reloadTab(newUrl){
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+            tabs.forEach(function (tab) {
+                if (tab.url.indexOf('telegram') !== -1) {
+                    console.log(tab.id);
+                    console.log(tab.url);
+                    chrome.tabs.remove(tab.id);
+                    chrome.tabs.create({ url: newUrl, pinned:true }, ()=>{
+                        setTimeout(()=>{
+                            console.log('Отправка скан');
+                            chrome.tabs.query({currentWindow: true}, function (tabs) {
+                                tabs.forEach(async function (tab) {
+                                    if (tab.url.indexOf('telegram') !== -1) {
+                                        chrome.tabs.sendMessage(tab.id, {oper: "Scan"});
+                                    }
+                                });
+                            });
+                            }, 5000);
+                    })
+                }
+            });
+        });
+    }
+    chrome.runtime.onMessage.addListener(async ({oper, listToServer}, sendResponse) => {
+        switch (oper) {
+            case "sendMessageBack": {
+                userChannel = listToServer;
+                break;
+            }
+            case "sendMessagePop": {
+                chrome.runtime.sendMessage({oper: 'sendMessageToPop', my_variable: userChannel}, function (response) {});
+                break;
+            }
+            case "CheckBox_Channel": {
+                tg_channel = listToServer;
+                console.log("Полученные тг :", tg_channel);
+                await reloadTab('https://web.telegram.org/z/#' + tg_channel[0]);
+                break;
+            }
+            case "Complited":{
+                tg_channel.shift()
+                if (tg_channel.length == 0){break}
+                else {await reloadTab('https://web.telegram.org/z/#' + tg_channel[0]);}
+                break;
+            }
+            default:
+                console.log('default');
         }
-        case "sendMessagePop":{
-            chrome.runtime.sendMessage({my_variable:userChannel},function (response){})
-            break;
-        }
-    }   
-        
-})
+    });
+    })();
--- public/content.js
+++ public/content.js
@@ -1,13 +1,30 @@
-const v = localStorage.getItem('tt-global-state'); // достаем данные из localStorage
-console.log(v)
-const v2 = JSON.parse(v)['chats']['byId']; // парсинг данных
-console.log(v2)
-let mas = new Array(); // создание массива каналов
-for (var i in v2){
-    if (v2[i]['id']<0){
-        mas.push(v2[i]['id']);
-    }
-    
+const data = localStorage.getItem('tt-global-state');
+const parseData = JSON.parse(data)['chats']['byId'];
+
+// Формирование массива каналов
+const channels = [];
+for (const chatId in parseData) {
+  if (parseData[chatId]['id'] < 0) {
+    const channel = [parseData[chatId]['id'], parseData[chatId]['title']];
+    channels.push(channel);
+  }
 }
-console.log(mas);
-chrome.runtime.sendMessage({oper:'sendMessage', listToServer: mas},function (response){})
+
+// Отправка сообщения в фоновую страницу
+chrome.runtime.sendMessage({ oper: 'sendMessageBack', listToServer: channels }, function(response) {});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse){
+    debugger;
+    switch(request.oper){
+      case "Scan" : {
+        console.log('Пришли в Scan');
+        setTimeout(function(){
+          chrome.runtime.sendMessage({ oper: 'Complited'});
+        }, 10000);
+        break;
+      }
+      default: {console.log('def')}
+    }
+  }
+);
